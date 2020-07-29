37ddfb095fcf19ecf15f58b2dd7efd2e58dd6de3
Neeraj Garg <njgarg22@gmail.com>
2020-07-29 22:16:31 +0530

    Invocation demo

---
+++ src/main/java/com/neeraj/rest/client/InvocationDemo.java
@@ -0,0 +1,27 @@
+package com.neeraj.rest.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Response;
+
+public class InvocationDemo {
+
+	public static void main(String[] args) {
+		InvocationDemo invocationDemo = new InvocationDemo();
+		Response resp = invocationDemo.prepareReqForMessagesByYear(2020).invoke();
+		System.out.println(resp.getStatus());
+
+	}
+	
+	public Invocation prepareReqForMessagesByYear(int year) {
+		Client client = ClientBuilder.newClient();
+
+		return client.target("http://localhost:8080/advanced-jaxrs/webapi/")
+				.path("messages")
+				.queryParam("year", year)
+				.request()
+				.buildGet();
+	}
+
+}
