1ce08f3227f6853d3a9f7dac41f2a23baef6e660
Neeraj Garg <njgarg22@gmail.com>
2020-07-29 21:50:13 +0530

    Improve rest client

--- src/main/java/com/neeraj/rest/client/RestApiClient.java
+++ src/main/java/com/neeraj/rest/client/RestApiClient.java
@@ -2,18 +2,36 @@ package com.neeraj.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+
 import com.neeraj.messenger.model.Message;
+import com.neeraj.messenger.model.Profile;
 
 public class RestApiClient {
 	
 	public static void main(String[] args) {
 		Client client = ClientBuilder.newClient();
-		Response response = client.target("http://localhost:8080/advanced-jaxrs/webapi/messages/1").request().get();
-		Message msg = response.readEntity(Message.class);
-		System.out.println(msg.getMessage());
 		
+		WebTarget baseTarget  = client.target("http://localhost:8080/advanced-jaxrs/webapi/");
+		WebTarget messagesTarget = baseTarget.path("messages");
+		WebTarget singleMsgTarget = messagesTarget.path("{messageID}");
+		
+		Message msg1 = singleMsgTarget
+				.resolveTemplate("messageID", "1")
+				.request()
+				.get(Message.class);
+		
+		Message msg2 = singleMsgTarget
+				.resolveTemplate("messageID", "2")
+				.request()
+				.get(Message.class);
+		
+		System.out.println(msg1.getMessage());
+		System.out.println(msg2.getMessage());
+
 	}
 	
 }
